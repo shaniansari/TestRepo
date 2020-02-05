e8b4125ee310d2a1021982a6472bf7ef8c72f549
shaniansari <shaniansari@hotmail.com>
2020-02-05 12:58:59 +0500

    Bio data of Zeeshan

---
+++ BioData-Zeeshan.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print(" ====== BIO DATA =====")
+print("Name : Zeeshan ")
+print("F. name = Waseem")
+print("Cell No: 0333123456")
+print("CNIC: 42501-90987655-5")
+print("Qualification: Bachelor")
+print("City: Karachi")
+
+
+# In[ ]:
+
+
+
+
